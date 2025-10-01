2e39ed952cdbdc1189c1bfdb52d856d8d199092b
Peltre <pedripjs@gmail.com>
2025-09-30 23:53:53 -0600

    Create Navbar component & add basic blueprint

---
+++ src/app/Components/Navbar.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+// Navbar component file, used to navigate through the portfolio
+// Last modified 01/10/2025 at 2:52pm
+
+const Navbar = () => {
+  return (
+    <div className='navbar bg-slate-400 shadow-md sticky top-0 z-50'>
+      <div className='navbar-start'>
+
+      </div>
+      <div className='navbar-center'>
+
+      </div>
+      <div className='navbar-end'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
