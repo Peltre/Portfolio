9a94a35ae32a92fabf3b3a5d333fe3afaf5fb4e9
Peltre <pedripjs@gmail.com>
2025-09-30 23:54:03 -0600

    Add component to main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Navbar from './Components/Navbar'
 
 const page = () => {
   return (
-    <div>
-      
-    </div>
+    <main>
+      <Navbar />
+    </main>
   )
 }
 
