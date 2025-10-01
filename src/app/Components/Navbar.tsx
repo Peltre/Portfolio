c83a4d89c9b77df0c35b678beea6743e9feead70
Peltre <pedripjs@gmail.com>
2025-10-01 00:08:24 -0600

    Create basic navbar structure

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from 'react'
+import { useRouter } from "next/navigation";
 // Navbar component file, used to navigate through the portfolio
 // Last modified 01/10/2025 at 2:52pm
 
 const Navbar = () => {
+    const router = useRouter();
+
   return (
     <div className='navbar bg-slate-400 shadow-md sticky top-0 z-50'>
-      <div className='navbar-start'>
+      <div className='navbar-start'>    
 
       </div>
-      <div className='navbar-center'>
-
+      <div className='navbar-center flex items-center justify-between'>
+        <button className='linkClassNavBar' onClick={() => router.push("/")}> HOME </button>
+        <button className='linkClassNavBar' onClick={() => router.push("/skills")}> SKILLS </button>
+        <button className='linkClassNavBar' onClick={() => router.push("/projects")}> PROJECTS </button>
+        <button className='linkClassNavBar' onClick={() => router.push("/contact")}> CONTACT </button>
       </div>
       <div className='navbar-end'>
 
