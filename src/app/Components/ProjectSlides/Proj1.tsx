9a49fa7e68e6c3af851f5ee2f0456afccadc2690
Peltre <pedripjs@gmail.com>
2025-10-03 07:47:24 -0600

    Update Project1, add project decoration to P1

--- src/app/Components/ProjectSlides/Proj1.tsx
+++ src/app/Components/ProjectSlides/Proj1.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 
-import backgroundAsset from "../../Assets/kod_bg_asset.png"
+import backgroundAsset from "../../Assets/KoD/kod_bg_asset.png"
+import LittleKnight from "../../Assets/KoD/LittleKnight.png"
+import Mago from "../../Assets/KoD/MagoIdle2.png"
+import FotoGallery from './FotoGallery'
+
+// File to display Project #1 
+// as of 03/10/2025 Proj #1 - Knigths of Dango
 
 const Proj1 = () => {
   return (
@@ -36,6 +42,27 @@ const Proj1 = () => {
                     Hola este es el baile del tilin
                     Hola este es el baile del tilin
                 </p>
+            </div>
+
+            {/* Image Decoration */}
+            <div className='absolute z-11 right-[40%] bottom-[1%]'>
+                <img 
+                    src={LittleKnight.src}
+                    alt='Pequeño Jit'
+                    className='pixelart w-50 h-50'
+                />
+            </div>
+
+            <div className='absolute z-11 right-[6%] bottom-[2%]'>
+                <img
+                    src={Mago.src}
+                    alt='Pequeño Jit Mago'
+                    className='pixelart w-50 h-50 transform scale-x-[-1]'
+                />
+            </div>
+
+            <div className='absolute z-10 right-[10%] w-[40%] h-[60%]'>
+                <FotoGallery />
             </div>
         </div>
     </div>
