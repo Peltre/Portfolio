61d2f225a7a20c011568667f07c01fb0b6954c78
Peltre <pedripjs@gmail.com>
2025-10-10 08:03:14 -0600

    Update ProjectSlide.tsx this component renders the project Title, desc & looping background
    - Will include Tools & Links

--- src/app/Components/ProjectSlides/ProjectSlide.tsx
+++ src/app/Components/ProjectSlides/ProjectSlide.tsx
@@ -1,12 +1,44 @@
 import React from 'react'
 import { ProjectData } from './types'
 
-const ProjectSlide = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+interface ProjectSlideProps {
+    project: ProjectData
 }
+
+const ProjectSlide: React.FC<ProjectSlideProps> = ({ project }) => {
+    return (
+        <div 
+            id={project.id}
+            className='carousel-item relative w-full h-150 overflow-hidden'
+        >
+            <div className='flex absolute w-[200%] h-full animate-slide'>
+            {/* Image Looping */}
+                <img
+                    src={project.imageSrc}
+                    alt='ProjectBg'
+                    className='w-1/2 h-full object-fit pixelart'
+                />
+
+                <img
+                    src={project.imageSrc}
+                    alt='ProjectBg'
+                    className='w-1/2 h-full object-fit pixelart'
+                />
+            </div>
+
+            {/* Overlay to darken image */}
+            <div className='absolute inset-0 bg-black/60 z-5'/>
+
+            {/* Text Content */}
+            <div className='absolute inset-0 flex flex-col items-start justify-center z-10 text-white px-12 left-[8%] bottom-[15%]'>
+                <div className='max-w-lg'>
+                    <h1 className='text-4xl font-bold mb-3'>{project.title}</h1>
+                    <p className='text-lg'>{project.description}</p>
+                </div>  
+            </div>
+        </div>
+    )
+}
+
 
 export default ProjectSlide
