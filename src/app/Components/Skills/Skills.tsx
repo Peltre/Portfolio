c908cca3d823b5494bbd0c258a9f939d42b7504b
Peltre <pedripjs@gmail.com>
2025-10-13 01:08:35 -0600

    Update Skills.tsx to implement skill selection to work with newly created SkillDetail

--- src/app/Components/Skills/Skills.tsx
+++ src/app/Components/Skills/Skills.tsx
@@ -1,16 +1,20 @@
-import React from 'react'
-import SkillBox from './SkillBox'
-import { skillsData  } from './Utils/skillsData'
+"use client";
 
+import React, { useState } from 'react'
+import SkillBox from './SkillBox'
+import SkillDetail from './SkillDetail'
+import { skillsData } from './Utils/skillsData'
+import { Skill } from './Utils/skill'
 
 const Skills = () => {
+    const [selectedSkill, setSelectedSkill] = useState<Skill | null>(skillsData[0])
   return (
     <div className='min-h-screen'>
       <div className='container mx-auto px-6 py-12 lg:py-16'>
-        {/* Text Container */}
-        <div className='grid grid-cols-2 items-center'>
+        <h1 className='text-white font-bold text-4xl'>Skills</h1>
+        {/* Left Side - Text Container */}
+        <div className='grid grid-cols-2 gap-8 items-center'>
             <div className='flex flex-col'>
-                <h1 className='text-white font-bold text-4xl'>Skills</h1>
                 <p className='text-lg pt-5 mb-10'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -22,10 +26,21 @@ const Skills = () => {
                         <SkillBox 
                             key={skill.id}
                             skill={skill}
+                            isSelected={selectedSkill?.id === skill.id}
+                            onClick={() => setSelectedSkill(skill)}
                         />
                     ))}
                 </div> 
             </div>
+
+            {/* Right Side - Skill Detail */}
+            <div className='flex items-center justify-center w-full'>
+                {selectedSkill && (
+                <div className='w-full max-w-md mx-auto'>
+                    <SkillDetail skill={selectedSkill} />
+                </div>
+                )}
+          </div>
         </div>
       </div>
     </div>
