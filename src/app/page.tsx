bc7351c46bb5c9ea8642ea1e5f8c4034d839a59a
Peltre <pedripjs@gmail.com>
2025-10-19 00:48:17 -0600

    add ProjectSelector to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ProjectCarousel from './Components/ProjectSlides/ProjectCarousel'
 import { projectsData } from './Components/ProjectSlides/Utils/projectsData'
 import Skills from './Components/Skills/Skills'
+import ProjectSelector from './Components/ProjectSelector/ProjectSelector'
 
 const page = () => {
   return (
     <main>
       <ProjectCarousel projects={projectsData}/>
       <Skills />
+      <ProjectSelector projects={projectsData}/>
     </main>
   )
 }
