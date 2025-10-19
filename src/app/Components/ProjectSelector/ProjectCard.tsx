b12bc19d39991bc89c2723e3cd367461a95a37a1
Peltre <pedripjs@gmail.com>
2025-10-19 00:28:53 -0600

    initial setup for project card component
    Recives a project item & a boolean indicating if its active

---
+++ src/app/Components/ProjectSelector/ProjectCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ProjectData } from '../ProjectSlides/Utils/types'
+
+interface ProjectCardProps {
+  project: ProjectData;
+  isActive: boolean;
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({ project, isActive })  => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProjectCard
